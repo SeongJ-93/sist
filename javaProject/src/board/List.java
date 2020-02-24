7828e52569624af17470e2d3c154eb70fabd6d3c
user <user@DESKTOP-COJ9858>
2020-02-24 10:35:33 +0900

    boarder 새로운 브랜치 - List, Write 구현 후 커밋

---
+++ javaProject/src/board/List.java
@@ -0,0 +1,10 @@
+package board;
+
+public class List {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("구현 중");
+	}
+
+}
---
+++ javaProject/src/board/Write.java
@@ -0,0 +1,10 @@
+package board;
+
+public class Write {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("쓰기 페이지 구현 중");
+	}
+
+}
