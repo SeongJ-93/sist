257a21c53150a4719ba2852cf6f9d2d10561b238
user <user@DESKTOP-COJ9858>
2020-02-24 10:38:33 +0900

    게시판 수정 , 삭제 완료

--- javaProject/src/board/Delete.java
+++ javaProject/src/board/Delete.java
@@ -1,10 +1,10 @@
 package board;
 
-public class Write {
+public class Delete {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("삭제 페이지 구현 중");
+		System.out.println("쓰기 페이지 구현 중");
 	}
 
 }
